fe98229981f624e729e14e0898b5992b8bac0fe1
J0hnyBG <ivan.parvanov1@gmail.com>
2017-04-17 23:13:22 +0300

    fixed build

--- SportsFeed/SportsFeed.WebClient.Tests/Controllers/HomeControllerTests.cs
+++ SportsFeed/SportsFeed.WebClient.Tests/Controllers/HomeControllerTests.cs
@@ -1,5 +1,13 @@
-﻿using NUnit.Framework;
+﻿using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
 
+using Moq;
+
+using NUnit.Framework;
+
+using SportsFeed.Services.Contracts;
 using SportsFeed.WebClient.Controllers;
 
 using TestStack.FluentMVCTesting;
@@ -10,13 +18,46 @@ namespace SportsFeed.WebClient.Tests.Controllers
     public class HomeControllerTests
     {
         [Test]
-        public void Index()
+        public void Index_ShouldReturnDefaultPartialView_WhenRequestIsAjax()
         {
             // Arrange
-            var controller = new HomeController();
+            var sport = string.Empty;
+            var sportsService = new Mock<ISportsService>();
+            var request = new Mock<HttpRequestBase>();
+            request.SetupGet(x => x.Headers).Returns(
+                                                     new WebHeaderCollection
+                                                     {
+                                                                           { "X-Requested-With", "XMLHttpRequest" }
+                                                     });
+
+            var context = new Mock<HttpContextBase>();
+            context.SetupGet(x => x.Request).Returns(request.Object);
+
+            var sut = new HomeController(sportsService.Object);
+            sut.ControllerContext = new ControllerContext(context.Object, new RouteData(), sut);
 
             // Act & Assert
-            controller.WithCallTo(c => c.Index())
+            sut.WithCallTo(c => c.Index(sport))
+                      .ShouldRenderPartialView("_Index");
+        }
+
+        [Test]
+        public void Index_ShouldReturnDefaultView_WhenRequestIsNotAjax()
+        {
+            // Arrange
+            var sport = string.Empty;
+            var sportsService = new Mock<ISportsService>();
+            var request = new Mock<HttpRequestBase>();
+            request.SetupGet(x => x.Headers).Returns(
+                                                     new WebHeaderCollection());
+            var context = new Mock<HttpContextBase>();
+            context.SetupGet(x => x.Request).Returns(request.Object);
+
+            var sut = new HomeController(sportsService.Object);
+            sut.ControllerContext = new ControllerContext(context.Object, new RouteData(), sut);
+
+            // Act & Assert
+            sut.WithCallTo(c => c.Index(sport))
                       .ShouldRenderDefaultView();
         }
     }
